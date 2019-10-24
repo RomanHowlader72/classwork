d68319925cbde1c987631e76f90a221a289ec338
RomanHowlader72 <56949210+RomanHowlader72@users.noreply.github.com>
2019-10-24 11:38:54 +0600

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,20 @@
+#Python for Loop
+#=====================================
+
+
+""""
+Syntax of for Loop
+
+for val in sequence:
+	Body of for
+"""
+
+for x in [7,33,5]:
+	print(x,end="\t") 
+	
+for x in range(0,99):
+	print(x,end="\t") 
+
+for x in [7,33,5]:
+	print(x) 
+
